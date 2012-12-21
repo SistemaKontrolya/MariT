64991a486a5bdb70867cbd42d35ff24eb9d3f553
Marina <madronnik@gmail.com>
2012-12-21 20:36:59 +0400

    убрала ошибку

--- TestPlus.ru/admin/admin_header.php
+++ TestPlus.ru/admin/admin_header.php
@@ -8,10 +8,13 @@ else {
 	SessionOff();
 }?>
 <header>ТЕСТИРОВАНИЕ +
-<div><a href="/admin">На главную</a></div>
 <div class="greeting">
 <?php Greeting($usr_name)?>
 </div>
 <form name="logout" method="GET" action="/Auth.php">
 <button type="submit" name="logout">Выйти</button>
 </form></header>
+<?
+if(($_SERVER['REQUEST_URI']!="/admin/index.php")&&($_SERVER['REQUEST_URI']!="/admin/"))
+	include("../menu.html");
+?>
